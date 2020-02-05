a726e06830ceef8ff65bbaf1e9974350eb1e59f4
Sarah Vivoli <snvivoli@gmail.com>
2020-02-05 09:27:37 -0800

    orm all function

--- config/orm.js
+++ config/orm.js
@@ -36,8 +36,14 @@ function objToSql(ob) {
 }
 
 const orm = {
-    selectAll: function () {
-
+    all: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+            if (err) {
+              throw err;
+            }
+            cb(result);
+          });
     },
 
     insertOne: function () {
