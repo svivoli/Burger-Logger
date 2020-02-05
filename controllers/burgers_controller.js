f900c6334a9af7e5338d0ff7b82ab2b87aaef4eb
Sarah Vivoli <snvivoli@gmail.com>
2020-02-05 09:32:09 -0800

    / route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,6 +3,14 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        const hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
 
 module.exports = router;
