b36a5505cdc64179f80c5d8b684a621b1052d734
Sarah Vivoli <snvivoli@gmail.com>
2020-02-05 09:24:16 -0800

    orm.js helper functions

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,4 +2,7 @@ var express = require("express");
 var router = express.Router();
 var burger = require("../models/burger.js");
 
+// Create all our routes and set up logic within those routes where required.
+
+
 module.exports = router;
